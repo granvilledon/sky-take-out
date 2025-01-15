15504c04204cb427e061222131ec7abc85ea7229
Granville Don <13393651+granvilledon@user.noreply.gitee.com>
2025-01-15 22:00:06 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -15,11 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/user/shoppingCart")
 @Slf4j
-@Api(tags = "C端-购物车接口")
+@Api(tags = "C端购物车相关接口")
 public class ShoppingCartController {
 
     @Autowired
     private ShoppingCartService shoppingCartService;
+
     /**
      * 添加购物车
      * @param shoppingCartDTO
@@ -27,8 +28,8 @@ public class ShoppingCartController {
      */
     @PostMapping("/add")
     @ApiOperation("添加购物车")
-    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
-        log.info("添加购物车，商品信息为： {}", shoppingCartDTO);
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车，商品信息为：{}",shoppingCartDTO);
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
     }
@@ -39,8 +40,7 @@ public class ShoppingCartController {
      */
     @GetMapping("/list")
     @ApiOperation("查看购物车")
-    public Result<List> list() {
-        log.info("查看购物车");
+    public Result<List<ShoppingCart>> list(){
         List<ShoppingCart> list = shoppingCartService.showShoppingCart();
         return Result.success(list);
     }
@@ -53,6 +53,19 @@ public class ShoppingCartController {
     @ApiOperation("清空购物车")
     public Result clean(){
         shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/sub")
+    @ApiOperation("删除购物车中一个商品")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("删除购物车中一个商品，商品：{}", shoppingCartDTO);
+        shoppingCartService.subShoppingCart(shoppingCartDTO);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -5,23 +5,20 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
-
 import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapper {
 
     /**
-     * 查询当前用户的购物车数据
-     *
+     * 动态条件查询
      * @param shoppingCart
      * @return
      */
     List<ShoppingCart> list(ShoppingCart shoppingCart);
 
     /**
-     * 插入购物车数据
-     *
+     * 根据id修改商品数量
      * @param shoppingCart
      */
     @Update("update shopping_cart set number = #{number} where id = #{id}")
@@ -29,18 +26,30 @@ public interface ShoppingCartMapper {
 
     /**
      * 插入购物车数据
-     *
      * @param shoppingCart
      */
     @Insert("insert into shopping_cart (name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
-            "values (#{name}, #{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{image}, #{createTime})")
+            " values (#{name},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{image},#{createTime})")
     void insert(ShoppingCart shoppingCart);
 
     /**
      * 根据用户id删除购物车数据
-     *
      * @param userId
      */
     @Delete("delete from shopping_cart where user_id = #{userId}")
     void deleteByUserId(Long userId);
+
+    /**
+     * 根据id删除购物车数据
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
+
+    /**
+     * 批量插入购物车数据
+     *
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
